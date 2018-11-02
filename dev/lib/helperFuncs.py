e92bdfee801fd6e9d174fc4744b7468c7e60525a
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-11-02 12:50:32 -0400

    add docstrings

--- dev/lib/helperFuncs.py
+++ dev/lib/helperFuncs.py
@@ -1,8 +1,13 @@
 """
 Helper functions for checkParallel
+They're here because I don't like having to scroll around too much
 """
 
 def calcSlope(pt1, pt2):
+    """
+    Given 2 points, calculate slope using m = y1 - y0 / x1 - x0
+    pt1 and pt2 should be a tuple each (x, y)
+    """
     (xA, yA) = pt1
     (xB, yB) = pt2
 
@@ -11,6 +16,21 @@ def calcSlope(pt1, pt2):
     except ZeroDivisionError:
         return 0 # Not entirely accurate for vertical lines, but works
 
+def areTheyParallel(line1, line2, tolerance=0):
+    """
+    Checks if 2 lines are parallel by comparing their slopes
+    line1 and line2 should be a tuple of tuples each: ((x0, y0), (x1, y1))
+    tolerance defaults to 0
+    """
+    ((x0, y0), (x1, y1)) = line1
+    ((x2, y2), (x3, y3)) = line2
+
+    m1 = calcSlope((x0, y0), (x1, y1))
+    m2 = calcSlope((x2, y2), (x3, y3))
+
+    # instead of checking for absolute equality (m1 == m2),
+    # allow for some tolerance
+    return abs(m1 - m2) <= tolerance
 
 def collectAllPointsInContour(contour):
     pointsList = []
