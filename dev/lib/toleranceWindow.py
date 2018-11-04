0fe0f6fd83ac085dd235aafeedded3b7f2aee5cc
Jesen Tanadi <mail@jesentanadi.com>
2018-11-04 13:30:04 -0500

    use accuracy in UI and convert to tolerance

--- dev/lib/toleranceWindow.py
+++ dev/lib/toleranceWindow.py
@@ -8,7 +8,7 @@ to notify it to read the setting whenever it changes.
 """
 
 import helperFuncs as hf
-from vanilla import FloatingWindow, Slider, Button
+from vanilla import FloatingWindow, Slider, Button, TextBox
 from mojo.events import postEvent
 from defconAppKit.windows.baseWindow import BaseWindowController
 import os.path
@@ -18,29 +18,56 @@ settingDir = os.path.join(currentDir, "..", "resources", "toleranceSetting.txt")
 
 class ToleranceWindow(BaseWindowController):
     def __init__(self):
-        self.w = FloatingWindow((150, 45),
-                                "Set tolerance")
-        self.w.toleranceSlider = Slider((10, 10, -10, 23),
+        """
+        Use "accuracy" in UI because it's easier to understand,
+        but convert to "tolerance" because it's easier to use
+        for parallel slope math later.
+        """
+        self.maxValue = 0.05
+        self.w = FloatingWindow((150, 60), "Set Accuracy")
+        self.w.accuracySlider = Slider((10, 9, -10, 23),
                                         minValue=0,
-                                        maxValue=0.050,
+                                        maxValue=self.maxValue,
                                         value=hf.readSetting(settingDir),
                                         sizeStyle="small",
-                                        callback=self.toleranceSliderCB)
+                                        callback=self.accuracySliderCB)
+        self.w.lessText = TextBox((10, 30, -10, 12),
+                                  text="Less",
+                                  sizeStyle="small")
+        self.w.moreText = TextBox((10, 30, -10, 12),
+                                  text="More",
+                                  alignment="right",
+                                  sizeStyle="small")
         self.setUpBaseWindowBehavior()
+        # Post this event so CheckParallel() can't open 2 ToleranceWindow()
         postEvent("comToleranceWindowOpened")
         self.w.open()
 
-    def toleranceSliderCB(self, sender):
-        toleranceValue = round(sender.get(), 3)
+    def accuracySliderCB(self, sender):
+        """
+        When slider changes, write setting and post event.
+        Reverse slider value by subtracting from maxValue
+        because we're tracking tolerance
+        """
+        toleranceValue = abs(round(self.maxValue - sender.get(), 3))
+        print(toleranceValue)
         self.writeSetting(toleranceValue)
         postEvent("comToleranceSettingChanged")
 
     def writeSetting(self, value):
-        settingFile = open(settingDir, "w")
+        """
+        Write setting to file or make new file if
+        setting file doesn't exist.
+        """
+        settingFile = open(settingDir, "w+")
         settingFile.write(str(value))
         settingFile.close()
 
     def windowCloseCallback(self, sender):
+        """
+        Let CheckParallel() know that ToleranceWindow() has been closed,
+        so CP() will let user open a TW() again.
+        """
         postEvent("comToleranceWindowClosed")
         super(ToleranceWindow, self).windowCloseCallback(sender)
 
