93e45ec8d995a88b4a8c64d7af3d5acb5cbbf77d
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-11-26 18:33:32 -0500

    move dwg functions out to a delegate obj

--- dev/lib/checkParallel.py
+++ dev/lib/checkParallel.py
@@ -29,30 +29,174 @@ KEYCODE = 44
 currentDir = os.path.dirname(__file__)
 settingDir = os.path.join(currentDir, "..", "resources", "toleranceSetting.txt")
 
-class CheckParallelTool(EditingTool):
+class DrawingDelegate:
+    """
+    A delegate object used by both ParallelGuides()
+    and EditParallelTool() to analyze segments and draw lines
+    """
     def __init__(self):
+        self.tolerance = hf.readSetting(settingDir)
+        self.selectedContours = {}
+        self.ptsFromSelectedCtrs = []
+
+    def analyzeAndGetPoints(self, glyph=None):
         """
-        Instantiate ToleranceWindow(), GuideStatusDisplay()
-        and some default values.
+        Analyze selection and get points
+        This method just calls 2 protected methods for easy access
         """
-        super().__init__()
-        self.toleranceWindow = ToleranceWindow()
+        if glyph is None:
+            return
+
+        self._analyzeSelection(glyph)
+        self._getPointsFromSelectedContours(glyph)
+
+    def draw(self, infoOrScale, glyph=None, lineWeightMultiplier=1):
+        """
+        Draw lines.
+
+        This method is called by both the observer watching
+        "draw" (turned on when "/" is pressed) and by the tool,
+        when it's active.
+
+        When used by the observer, it returns "info", a dict
+        from which we need to grab scale. When used by the tool,
+        it returns scale, so we can use it right away.
+        """
+        # This is just for naming...
+        scale = infoOrScale
+        if isinstance(infoOrScale, dict) and glyph is None:
+            scale = infoOrScale["scale"]
+            glyph = infoOrScale["glyph"]
+
+        # Just in case...
+        if glyph is None:
+            return
+
+        # Also do this here in case mouseDown isn't fired
+        # (eg. user uses keyboard to select segments)
+        self.analyzeAndGetPoints(glyph)
+
+        # Only draw if something has been selected
+        if not self.ptsFromSelectedCtrs:
+            return
+
+        for cluster in self.ptsFromSelectedCtrs:
+            # Don't draw if there are overlapping points
+            # The set comprehension will remove duplicate points
+            # (ie. tuple & set won't have same lengths)
+            clusterPosSet = {point.position for point in cluster}
+            if len(cluster) != len(clusterPosSet):
+                continue
+
+            pt0, pt1, pt2, pt3 = cluster[0].position, cluster[1].position,\
+                                 cluster[2].position, cluster[3].position
+
+            # Parallel-ish lines are green; otherwise, red
+            if hf.areTheyParallel((pt0, pt1), (pt2, pt3), self.tolerance):
+                dt.stroke(0, 1, 0, 1)
+            else:
+                dt.stroke(1, 0, 0, 1)
+
+            dt.strokeWidth(scale)
+            dt.line(pt0, pt1)
+
+            dt.strokeWidth(scale * lineWeightMultiplier)
+            dt.line(pt2, pt3)
+
+    def readToleranceSetting(self):
+        """
+        Read tolerance setting from file
+        """
+        self.tolerance = hf.readSetting(settingDir)
+
+    def _analyzeSelection(self, glyph):
+        """
+        Look at what's selected and add appropriate segment(s)
+        to the self.selectedContours dict.
+
+        We don't explicitly check if a segment is a curve because
+        we don't draw segments without offcurves in draw() anyway.
+        """
+        self.selectedContours.clear()
+
+        # Find which segments in each contour are selected
+        for contour in glyph:
+            selectedSegments = []
+
+            for segment in contour:
+                if segment.selected:
+                    selectedSegments.append(segment)
+
+                for point in segment:
+                    # Treat offcurve selection normally (only add current segment)
+                    if point.selected and point.type == "offcurve":
+                        selectedSegments.append(segment)
+
+                    # If an oncurve is selected, add current and next segments
+                    # so user can balance pt between 2 segments
+                    elif point.selected:
+                        # If any point adjacent to current point is selected, then
+                        # a segment has been selected, and it's been taken care of above
+                        # This prevents 2 segments from being selected when user
+                        # selects a segment.
+                        if hf.findPrevPt(point, contour).selected\
+                        or hf.findNextPt(point, contour).selected:
+                            continue
+
+                        # If it's the last segment (no next index), add first segment
+                        try:
+                            selectedSegments.append(contour[segment.index + 1])
+                        except IndexError:
+                            selectedSegments.append(contour[0])
+
+                        selectedSegments.append(segment)
+
+            if selectedSegments:
+                self.selectedContours[contour.index] = selectedSegments
+
+    def _getPointsFromSelectedContours(self, glyph):
+        """
+        Append all selected points (oncurves & bcps)
+        as a list of tuples of tuples in self.ptsFromSelectedCtrs
+        [
+            ((x0, y0), (x1, y1), (x2, y2), (x3, y3)),
+            ((x4, y4), (x5, y5), (x6, y6), (x7, y7)),
+            ...
+        ]
+
+        **Actual points are appended, not positions**
+        """
+        self.ptsFromSelectedCtrs.clear()
+
+        for index, selectedSegments in self.selectedContours.items():
+            currentContour = glyph[index]
+            for segment in selectedSegments:
+                selectedOnCurves = [point for point in segment.points if point.type != "offcurve"]
+                selectedOffCurves = [point for point in segment.points if point.type == "offcurve"]
+
+                # If no selectedOffCurves, it's a straight line, so ignore
+                if not selectedOffCurves:
+                    continue
+
+                pt0 = hf.findPrevPt(selectedOnCurves[0], currentContour)
+                pt1 = selectedOnCurves[0]
+                pt2 = selectedOffCurves[0]
+                pt3 = selectedOffCurves[1]
+
+                self.ptsFromSelectedCtrs.append((pt0, pt1, pt2, pt3))
+
+
+class ParallelGuides:
+    """
+    Show parallel guides outside of the EditParallelTool
+    This class adds observers that last an entire RF session
+    """
+    def __init__(self, delegate):
+        self.delegate = delegate
         self.guideStatus = GuideStatusView()
 
-        self.tolerance = hf.readSetting(settingDir)
-        self.glyph = None
+        self.displayGuides = False
 
-        # Use a dict so we can keep track of
-        # what each selectedSegment belongs to
-        self.selectedContours = {}
-        self.ptsFromSelectedCtrs = None
-        self.lineWeightMultiplier = 1
-        self.mouseDownPoint = None
-
-        self.canMarquee = True
-        self.nonToolShouldDraw = False
-
-        # These observers are always watching RF
         addObserver(self, "keyDownCB", "keyDown")
         addObserver(self, "glyphWindowOpenCB", "glyphWindowDidOpen")
 
@@ -75,26 +219,42 @@ class CheckParallelTool(EditingTool):
         if keyCode != KEYCODE:
             return
 
-        self.nonToolShouldDraw = not self.nonToolShouldDraw
+        self.displayGuides = not self.displayGuides
 
-        if self.nonToolShouldDraw:
-            self.glyph = CurrentGlyph()
+        if self.displayGuides:
             self.guideStatus.turnStatusTextOn()
-            addObserver(self, "draw", "draw")
-            addObserver(self, "updateGlyphCB", "currentGlyphChanged")
+            addObserver(self, "drawCB", "draw")
         else:
             self.guideStatus.turnStatusTextOff()
             removeObserver(self, "draw")
-            removeObserver(self, "currentGlyphChanged")
 
         UpdateCurrentGlyphView()
 
-    def updateGlyphCB(self, info):
+    def drawCB(self, info):
         """
-        Update self.glyph when currentGlyphChanged
-        Instead of self.glyph = CurrentGlyph() at draw()
+        Pass on to delegate method
         """
-        self.glyph = info["glyph"]
+        self.delegate.draw(info)
+
+
+class EditParallelTool(EditingTool):
+    """
+    Tool to edit line connecting BCPs.
+    Works kind of like Tunni tools.
+    """
+    def __init__(self, delegate):
+        super().__init__()
+        self.delegate = delegate
+        self.toleranceWindow = ToleranceWindow()
+
+        self.glyph = None
+
+        # Use a dict so we can keep track of
+        # what each selectedSegment belongs to
+        self.lineWeightMultiplier = 1
+        self.mouseDownPoint = None
+
+        self.canMarquee = True
 
     def getToolbarIcon(self):
         """
@@ -144,14 +304,13 @@ class CheckParallelTool(EditingTool):
         # outside of the contours (eg. on the BCP connection)
         # and we will have no selections to analyze.
         self._selectSegmentWhenBCPConnectionIsClicked()
-        self._analyzeSelection()
-        self.ptsFromSelectedCtrs = self._getPointsFromSelectedContours()
+        self.delegate.analyzeAndGetPoints()
 
-        if not self.ptsFromSelectedCtrs:
+        if not self.delegate.ptsFromSelectedCtrs:
             return
 
         # Not all pts are necessary, but here for consistency
-        for cluster in self.ptsFromSelectedCtrs:
+        for cluster in self.delegate.ptsFromSelectedCtrs:
             self.pt0, self.pt1, self.pt2, self.pt3 = cluster[0], cluster[1],\
                                                      cluster[2], cluster[3]
 
@@ -180,7 +339,7 @@ class CheckParallelTool(EditingTool):
         self.glyph.prepareUndo("Move handles")
 
         # For now, only allow editing when one segment is selected
-        if len(self.ptsFromSelectedCtrs) != 1:
+        if len(self.delegate.ptsFromSelectedCtrs) != 1:
             return
         if self.mouseDownPoint is None:
             return
@@ -249,136 +408,18 @@ class CheckParallelTool(EditingTool):
             return
         super().dragSelection(point, delta)       
 
-    def draw(self, infoOrScale):
+    def draw(self, scale):
         """
-        Draw lines.
-
-        This method is called by both the observer watching
-        "draw" (turned on when "/" is pressed) and by the tool,
-        when it's active.
-
-        When used by the observer, it returns "info", a dict
-        from which we need to grab scale. When used by the tool,
-        it returns scale, so we can use it right away.
+        Pass drawing to delegate object's method
         """
-        # This is just for naming...
-        scale = infoOrScale
-        if isinstance(infoOrScale, dict):
-            scale = infoOrScale["scale"]
-
-        # Also do these here in case mouseDown isn't fired
-        # (eg. user uses keyboard to select segments)
-        self._analyzeSelection()
-        self.ptsFromSelectedCtrs = self._getPointsFromSelectedContours()
-
-        # Only draw if something has been selected
-        if not self.ptsFromSelectedCtrs:
-            return
-
-        for cluster in self.ptsFromSelectedCtrs:
-            # Don't draw if there are overlapping points
-            # The set comprehension will remove duplicate points
-            # (ie. tuple & set won't have same lengths)
-            clusterPosSet = {point.position for point in cluster}
-            if len(cluster) != len(clusterPosSet):
-                continue
-
-            pt0, pt1, pt2, pt3 = cluster[0].position, cluster[1].position,\
-                                 cluster[2].position, cluster[3].position
-
-            # Parallel-ish lines are green; otherwise, red
-            if hf.areTheyParallel((pt0, pt1), (pt2, pt3), self.tolerance):
-                dt.stroke(0, 1, 0, 1)
-            else:
-                dt.stroke(1, 0, 0, 1)
-
-            dt.strokeWidth(scale)
-            dt.line(pt0, pt1)
-
-            dt.strokeWidth(scale * self.lineWeightMultiplier)
-            dt.line(pt2, pt3)
+        self.delegate.draw(scale, self.glyph, self.lineWeightMultiplier)
 
     def _applyTolerance(self, info):
         """
+        Pass to delegate object's method:
         Redefine tolerance whenever com.ToleranceSettingChanged is triggered
         """
-        self.tolerance = hf.readSetting(settingDir)
-
-    def _analyzeSelection(self):
-        """
-        Look at what's selected and add appropriate segment(s)
-        to the self.selectedContours dict.
-
-        We don't explicitly check if a segment is a curve because
-        we don't draw segments without offcurves in draw() anyway.
-        """
-        self.selectedContours.clear()
-
-        # Find which segments in each contour are selected
-        for contour in self.glyph:
-            selectedSegments = []
-
-            for segment in contour:
-                if segment.selected:
-                    selectedSegments.append(segment)
-
-                for point in segment:
-                    # Treat offcurve selection normally (only add current segment)
-                    if point.selected and point.type == "offcurve":
-                        selectedSegments.append(segment)
-
-                    # If an oncurve is selected, add current and next segments
-                    # so user can balance pt between 2 segments
-                    elif point.selected:
-                        # If any point adjacent to current point is selected, then
-                        # a segment has been selected, and it's been taken care of above
-                        # This prevents 2 segments from being selected when user
-                        # selects a segment.
-                        if hf.findPrevPt(point, contour).selected\
-                        or hf.findNextPt(point, contour).selected:
-                            continue
-
-                        # If it's the last segment (no next index), add first segment
-                        try:
-                            selectedSegments.append(contour[segment.index + 1])
-                        except IndexError:
-                            selectedSegments.append(contour[0])
-
-                        selectedSegments.append(segment)
-
-            if selectedSegments:
-                self.selectedContours[contour.index] = selectedSegments
-
-    def _getPointsFromSelectedContours(self):
-        """
-        Return all selected points (oncurves & bcps)
-        as a list of tuples of tuples
-        [
-            ((x0, y0), (x1, y1), (x2, y2), (x3, y3)),
-            ((x4, y4), (x5, y5), (x6, y6), (x7, y7)),
-            ...
-        ]
-
-        **Actual points are returned, not positions**
-        """
-        selectedPoints = []
-        for index, selectedSegments in self.selectedContours.items():
-            currentContour = self.glyph[index]
-            for segment in selectedSegments:
-                selectedOnCurves = [point for point in segment.points if point.type != "offcurve"]
-                selectedOffCurves = [point for point in segment.points if point.type == "offcurve"]
-
-                # If no selectedOffCurves, it's a straight line, so ignore
-                if not selectedOffCurves:
-                    continue
-
-                pt0 = hf.findPrevPt(selectedOnCurves[0], currentContour)
-                pt1 = selectedOnCurves[0]
-                pt2 = selectedOffCurves[0]
-                pt3 = selectedOffCurves[1]
-
-                selectedPoints.append((pt0, pt1, pt2, pt3))
-        return selectedPoints
+        self.delegate.readToleranceSetting()
 
     def _selectSegmentWhenBCPConnectionIsClicked(self):
         """
@@ -388,7 +429,7 @@ class CheckParallelTool(EditingTool):
         If multiple segments are selected, only one
         segment will remain selected
         """
-        for selectedSegments in self.selectedContours.values():
+        for selectedSegments in self.delegate.selectedContours.values():
             for segment in selectedSegments:
                 offCurves = [point for point in segment if point.type == "offcurve"]
                 pt0Pos = offCurves[0].position
@@ -402,4 +443,8 @@ class CheckParallelTool(EditingTool):
                 segment.selected = True
 
 
-installTool(CheckParallelTool())
+dwgDelegate = DrawingDelegate()
+parallelGuides = ParallelGuides(dwgDelegate)
+parallelTool = EditParallelTool(dwgDelegate)
+
+installTool(parallelTool)
